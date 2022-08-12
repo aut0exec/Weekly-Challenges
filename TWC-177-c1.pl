d07e705501c442104f1129f637103ada8a2d1eda
aut0exec <aut0exec@local>
2022-08-11 20:54:09 -0400

    adds solution to palindromic prime cyclops

---
+++ TWC-177-c1.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/env perl
+# 
+# Write a script to generate first 20 Palindromic Prime Cyclops Numbers.
+# A cyclops number is a number with an odd number of digits that has a zero in the center only.
+#
+# Output
+# 101, 16061, 31013, 35053, 38083, 73037, 74047, 91019, 94049, 1120211, 1150511, 1160611, 1180811, 1190911, 1250521, 1280821, 1360631, 1390931, 1490941, 1520251
+#
+# Not sure why 1300031 isn't a Palindromic prime Cyclops per the challenge; This code thinks that it is and everything I'm seeing online it should be...
+# -- Follow up... It's not Cycloptic! 
+
+use strict;
+use warnings;
+
+my @primes = ('2');
+
+sub check_cyclops {
+	
+	my $check_val = shift;
+	
+	foreach my $prime ( @primes ) {
+		if ( $check_val % $prime == 0 ) { return 0; }
+	}
+	
+	return 1;
+}
+
+foreach (3..1600000) {
+	
+	if ($_ == reverse($_)) {
+		# If number ends in these it's not prime
+		my @nums = split(//, $_);
+		if ( $nums[-1] =~ /0|2|4|5|6|8/) { next; }
+
+		# If the sum of the digits is divisble by 3, it's not prime
+		my $sum = 0;
+		$sum += $_ for split(//, $_);;
+		if ( $sum % 3 == 0) { next; } 
+
+		# Check that middle digit is 0, length isn't even, that there aren't multiple zero in number, and that value is prime
+		my $dig_length = length($_);
+		if ( 0 == $nums[int((($dig_length/2)))] and 0 ne $dig_length % 2 and check_cyclops $_ and not $_ =~ /0{2,}/ ) { 		
+			print ("$_ is a palindromic prime cyclops number!\n"); 
+		} 
+	}
+	
+	if ( $_ < 10000) {
+		# Find primes to be used later to check palindromes
+		my $i;
+		for ( $i=3; $i < $_; $i++) {
+			#~ if ( ($_ % $i) == 0 or ($_ % 2) == 0) { last; }
+			if ( ($_ % $i) == 0 ) { last; }
+		}
+		if ( $i eq $_ ) { push (@primes, $_); }
+	}
+}
